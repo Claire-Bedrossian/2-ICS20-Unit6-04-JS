7edcb43466dd9acf3547a02c2eec14176d95d39b
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2023-01-02 15:08:05 -0500

    added function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
-// Copyright (c) 202X Claire Bedrossian All rights reserved
+// Copyright (c) 2023 Claire Bedrossian All rights reserved
 //
 // Created by: Claire Bedrossian
-// Created on: XXX 202X
+// Created on: Jan 2023
 // This file contains the JS functions for index.html
 
 "use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/2-ICS20-Unit6-04-JS/sw.js", {
+    scope: "/2-ICS20-Unit6-04-JS/",
   })
 }
+window.onload = function () {
+  // this calculates the volume of the sphere
+  const params = new URLSearchParams(document.location.search)
 
-/**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
-}
+  //input
+  const length = parseFloat(params.get("l"))
+  const width = parseFloat(params.get("w"))
+
+  //process
+  const area = length * width
+  const perimeter = length * 2 + width * 2
+
+  //output
+  document.getElementById("perimeter").innerHTML =
+    "<h6>The perimeter is equal to: " + perimeter + "cm²</h6)"
+  document.getElementById("area").innerHTML =
+    "<h6>The area is equal to: " + area.toFixed(2) + " cm</h6>"
